a32382ba871b083cf1a1c8d93da848a4c073a789
Vaibhav Ahire <59802841+vibhi99@users.noreply.github.com>
2021-09-27 10:25:17 +0530

    Add files via upload

---
+++ Express_CRUD/Members.js
@@ -0,0 +1,37 @@
+const members = [
+    {   
+        id: 1,
+        roll_number: 1,
+        name: 'Rahul Wahi',
+        status: 'active',
+    },
+
+    {   
+        id: 2,
+        roll_number: 2,
+        name: 'Naveen Bansal',
+        status: 'active',
+    },
+    {   
+        id: 3,
+        roll_number: 3,
+        name: 'John Lage',
+        status: 'active',
+    },
+    {   
+        id: 4,
+        roll_number: 4,
+        name: 'Rohan Joshi',
+        status: 'inactive',
+    },
+    {   
+        id: 5,
+        roll_number: 5,
+        name: 'Aditya Singh',
+        status: 'active',
+    }
+];
+
+
+
+module.exports= members;
