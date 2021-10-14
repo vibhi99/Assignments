c67dea6a437a9b1f998755ab0acc8f4e73cd812b
Vaibhav Ahire <59802841+vibhi99@users.noreply.github.com>
2021-10-14 14:17:17 +0530

    Update employee.js

--- EmployeeDB_Api/routes/employee.js
+++ EmployeeDB_Api/routes/employee.js
@@ -3,10 +3,11 @@ const router = express.Router()
 
 const EmployeeController = require('../controllers/EmployeeController')
 const AuthController = require('../controllers/AuthController')
-const authenticate = require('../middleware/authenticate')
+const {authenticate, restrictTO} = require('../middleware/authenticate')
 
 
-router.get('/', authenticate, 
+router.get('/', authenticate,
+        restrictTO('admin'),
         EmployeeController.index)
         
 router.post('/show', authenticate, EmployeeController.show)
