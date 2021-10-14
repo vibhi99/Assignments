c044f6e813570c3a41e0cf9884d1293c50be04a0
Vaibhav Ahire <59802841+vibhi99@users.noreply.github.com>
2021-10-14 14:15:12 +0530

    Update AuthController.js

--- EmployeeDB_Api/controllers/AuthController.js
+++ EmployeeDB_Api/controllers/AuthController.js
@@ -44,7 +44,7 @@ const login = (req, res, next) =>{
                     })
                 }
                 if(result){
-                    let token = jwt.sign({name: user.name}, 'verySecretValue', {expiresIn: '1h'})
+                    let token = jwt.sign({name: user.name, role: user.role}, 'verySecretValue', {expiresIn: '1h'})
                     res.json({
                         message: 'Login Successfull!',
                         token
